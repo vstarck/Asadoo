2792d8ee93030130056f6424429647541b5812b4
Aijoona <vstarck@ienter-dev-valen.ienter.local>
2011-09-29 14:40:18 -0300

    Rename

---
+++ asadoo/core/Container.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @auhtor Fabien Potencer
+ * @see http://www.slideshare.net/fabpot/dependency-injection-with-php-53
+ * @throws InvalidArgumentException
+ */
+class Container {
+    protected $values = array();
+
+    function __set($id, $value) {
+        $this->values[$id] = $value;
+    }
+
+    function __get($id) {
+        if (!isset($this->values[$id])) {
+            throw new InvalidArgumentException(sprintf('Value "%s" is not defined.', $id));
+        }
+        if (is_callable($this->values[$id])) {
+            return $this->values[$id]($this);
+        } else {
+            return $this->values[$id];
+        }
+    }
+
+    function asShared($callable) {
+        return function ($c) use ($callable) {
+            static $object;
+            if (is_null($object)) {
+                $object = $callable($c);
+            }
+            return $object;
+        };
+    }
+}
