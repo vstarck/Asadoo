eff6ee1789a870b80f0019d8e5cdb148a2f02581
Valentin Starck <valentin.starck@gmail.com>
2012-01-12 17:22:48 -0300

    Format

--- examples/basic/index.php
+++ examples/basic/index.php
@@ -3,22 +3,22 @@
 include '../../dist/asadoo.php';
 
 asadoo()
-->on('/view/:id')
-->on('/view')
-->handle(function($request, $response, $dependences) {
-$id = $request->get('id', 'ID not found!');
+    ->on('/view/:id')
+    ->on('/view')
+    ->handle(function($request, $response, $dependences) {
+        $id = $request->get('id', 'ID not found!');
 
-$response->send($id);
-})
-->close();
+        $response->send($id);
+    })
+    ->close();
 
 
 asadoo()
-->on('*')
-->handle(function($request, $response, $dependences) {
-$response->header404();
-$response->send('404');
-})
-->close();
+    ->on('*')
+    ->handle(function($request, $response, $dependences) {
+        $response->header404();
+        $response->send('404');
+    })
+    ->close();
 
 asadoo()->start();
