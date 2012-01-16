11c70ff4c697cce2126e0ad78f57c9bf1d9b5c9f
Valentin Starck <valentin.starck@gmail.com>
2012-01-16 20:59:30 -0300

    Added methods: isGet, get, post and value

--- src/AsadooRequest.php
+++ src/AsadooRequest.php
@@ -6,7 +6,7 @@ class AsadooRequest {
         return strpos($this->url(), $match) !== false;
     }
 
-    public function get($key, $fallback = null) {
+    public function value($key, $fallback = null) {
         if (isset($this->variables[$key])) {
             return $this->variables[$key];
         }
@@ -18,21 +18,41 @@ class AsadooRequest {
         return $fallback;
     }
 
+    public function post($key, $fallback = null) {
+        if (isset($_POST[$key])) {
+            return $_POST[$key];
+        }
+
+        return $fallback;
+    }
+
+    public function get($key, $fallback = null) {
+        if (isset($_GET[$key])) {
+            return $_GET[$key];
+        }
+
+        return $fallback;
+    }
+
     public function set($key, $value = null) {
         if (is_array($key)) {
             foreach ($key as $k => $v) {
                 $this->set($k, $v);
             }
-            return;
+            return $this;
         }
 
         $this->variables[$key] = $value;
 
-        return $value;
+        return $this;
     }
 
     public function isPost() {
         return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+
+    public function isGet() {
+        return $_SERVER['REQUEST_METHOD'] == 'GET';
     }
 
     public function url() {
